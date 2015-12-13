722ab961c64dd0273baf3846d196a04cf437551d
callejac <clint.calleja@tipico.com>
2015-12-13 09:34:11 +0100

    Performing calculation on each receipt update to bind fields with a property instead of a function performing calculations on each digest cycle

--- app/stock-items-view/stock-items-view.js
+++ app/stock-items-view/stock-items-view.js
@@ -13,6 +13,10 @@ angular.module('stock.stockItems', ['ngRoute', 'angular.third.party.module'])
   }])
 
   .controller('StockItemsCtrl', ['$scope', 'angThirdParty', function ($scope, angThirdParty) {
+
+    $scope.total = 0;
+    $scope.stockItems = {};
+
     //get list of stok items
     angThirdParty.then(function (currentStockData) {
       $scope.stockItems = currentStockData;
@@ -38,50 +42,44 @@ angular.module('stock.stockItems', ['ngRoute', 'angular.third.party.module'])
     /**
      *
      * @param item
+     * @returns {number}
+     */
+    $scope.calculateSubtotal = function (item) {
+      item.subtotal = (item.price * item.cartQty);
+    }
+
+    /**
+     *
+     * @param item
+     * @returns {number}
+     */
+    $scope.calculateTotal = function () {
+
+      $scope.total = 0;
+      for (var index in $scope.stockItems){
+        var item = $scope.stockItems[index];
+        if(item.cartQty > 0){
+          $scope.total += item.price * item.cartQty;
+        }
+      }
+    }
+
+    /**
+     *
+     * @param item
      * @param increase
      */
     $scope.updateItemCartQuantity = function (item, increase) {
       if (item) {
         if (increase === true) {
           item.cartQty += 1;
-          //todo - check if there is enough in stock otherwise grey out button
         } else if(item.cartQty != 0){
           item.cartQty -= 1;
         }
 
-
-        //$scope.calculateSubtotal(item);
-        //$scope.calculateTotal();
+        //calculate these after each item update
+        $scope.calculateSubtotal(item);
+        $scope.calculateTotal();
       }
-
-      /**
-       *
-       * @param item
-       * @returns {number}
-       */
-      $scope.calculateSubtotal = function (item) {
-        return item.price * item.cartQty;
-
-      }
-
-      /**
-       *
-       * @param item
-       * @returns {number}
-       */
-      $scope.calculateTotal = function () {
-
-        var total = 0;
-        for (var index in $scope.stockItems){
-          var item = $scope.stockItems[index];
-          if(item.cartQty > 0){
-            total += item.price * item.cartQty;
-          }
-        }
-
-        return total;
-
-      }
-
     }
   }]);
